671feb564c0149afaeaa4ab25e17b513af360730
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-19 22:28:13 +0200

    Add a rectangle class and call it calculating area

--- L_SOLID.cs
+++ L_SOLID.cs
@@ -1,9 +1,40 @@
 ﻿using static System.Console;
-public class Demo
-{
-    static void Main(string[] args)
-    {
-        WriteLine("Liskov Substitution");
 
+namespace DotNetDesignPatternDemos.SOLID.LiskovSubstitutionPrinciple
+{
+    // using a classic example
+    public class Rectangle
+    {
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
     }
-}
+
+        public class Demo
+        {
+            static public int Area(Rectangle r) => r.Width * r.Height;
+
+            static void Main(string[] args)
+            {
+                Rectangle rc = new Rectangle(2, 3);
+                WriteLine($"{rc} has area {Area(rc)}");
+
+            }
+        }
+    }
