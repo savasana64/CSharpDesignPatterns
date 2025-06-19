d69ae574fc7b0ca4c0b3ba1afd27e106edb10049
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-19 20:15:29 +0200

    Add a class for adding a removing entries to a journal

--- Demo.cs
+++ Demo.cs
@@ -1,9 +1,38 @@
 ﻿using static System.Console;
 
+
+namespace DotNetDesignPatternDemos.SOLID.SRP
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
 public class Demo
 {
-    static void Main(string[] args)
-    {
-        WriteLine("Demo");
-    }
+   static void Main(string[] args)
+   {
+          var j = new Journal();
+          j.AddEntry("I cried today");
+          j.AddEntry("I ate a bug");
+          WriteLine("Demo");
+   }
+  }
 }
