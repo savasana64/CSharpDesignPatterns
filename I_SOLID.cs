71afd4feb5e18c1bb2001f82a81c38bda840e5e3
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-19 23:01:11 +0200

    Fix violation to interface segregation principle

--- I_SOLID.cs
+++ I_SOLID.cs
@@ -32,6 +32,73 @@ namespace DotNetDesignPatternDemos.SOLID.InterfaceSegregationPrinciple
         }
     }
 
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Printer : IPrinter
+    {
+        public void Print(Document d)
+        {
+
+        }
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner //An interface can implement other interafces as well
+    {
+
+    }
+
+    public struct MultiFunctionMachine : IMultiFunctionDevice
+    {
+        // compose this out of several modules
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d); // delegate to the printer
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d); // delegate to the scanner
+        }
+    }
+
     public class OldFashionedPrinter : IMachine
     {
         public void Print(Document d)
