eddf8d3b51f97c0cc194cfa6c97b7a9556b22b89
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-19 22:34:53 +0200

    Add files via upload

--- L_SOLID.cs
+++ L_SOLID.cs
@@ -26,6 +26,19 @@ namespace DotNetDesignPatternDemos.SOLID.LiskovSubstitutionPrinciple
         }
     }
 
+    public class Square : Rectangle
+    {
+        //public new int Width
+        //{
+        //  set { base.Width = base.Height = value; }
+        //}
+
+        //public new int Height
+        //{ 
+        //  set { base.Width = base.Height = value; }
+        //}
+    }
+
         public class Demo
         {
             static public int Area(Rectangle r) => r.Width * r.Height;
@@ -35,6 +48,16 @@ namespace DotNetDesignPatternDemos.SOLID.LiskovSubstitutionPrinciple
                 Rectangle rc = new Rectangle(2, 3);
                 WriteLine($"{rc} has area {Area(rc)}");
 
-            }
+            // should be able to substitute a base type for a subtype
+            /*Square*/
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            WriteLine($"{sq} has area {Area(sq)}");
+            // but this will not work as expected
+            // because the square is not a rectangle in the sense of Liskov Substitution Principle
+            // WriteLine($"{sq} has area {Area(sq)}");
+            // so we need to fix the Square class to ensure it adheres to the Liskov Substitution Principle
+
         }
+    }
     }
