60c76aca083bd84846e6a5361d2c603783b48a3e
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-21 09:47:03 +0200

    Add a simple way to research through relations
    This a bad way because we are accessing the low-level property directly. The relatioship class cannot change its way in which it stores data like for exampe using a dictionary instead of tuple.

--- D_SOLID.cs
+++ D_SOLID.cs
@@ -1,9 +1,74 @@
-﻿using static System.Console;
-public class Demo
-{
-    static void Main(string[] args)
-    {
-        WriteLine("Dependency Inversion Principle");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
+namespace DotNetDesignPatternDemos.SOLID.DependencyInversionPrinciple
+{
+    // high level modules should not depend on low-level; both should depend on abstractions
+    // abstractions should not depend on details; details should depend on abstractions
+
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+        // public DateTime DateOfBirth;
+    }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    public class Relationships // low-level
+    {
+        private List<(Person, Relationship, Person)> relations
+          = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    public class Research
+    {
+        public Research(Relationships relationships)
+        {
+            //high - level: find all of john's children
+            var relations = relationships.Relations;
+            foreach (var r in relations
+              .Where(x => x.Item1.Name == "John"
+                          && x.Item2 == Relationship.Parent))
+            {
+                WriteLine($"John has a child called {r.Item3.Name}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Matt" };
+
+            // low-level module
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            //we want to acces our low-level module in a high level modiúle (Research)
+            //first way: use the low-level module directly (creating a constructor that takes the relashionships as argument)
+
+            new Research(relationships);
+
+        }
     }
 }
