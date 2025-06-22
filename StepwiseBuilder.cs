1a00dedd719b2ed35034f393f22bd7b28a9a793e
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-22 15:34:35 +0200

    Add stepwiseBuilder
    Kind of usage of interface segregation.
    A chain enforced through a chain of interfaces.

--- StepwiseBuilder.cs
+++ StepwiseBuilder.cs
@@ -1,4 +1,7 @@
-﻿namespace StepwiseBuilder
+﻿using System;
+using System.Collections.Immutable;
+
+namespace StepwiseBuilder
 {
     public enum CarType
     {
@@ -10,11 +13,70 @@
         public CarType Type;
         public int WheelSize;
     }
+
+    public interface ISpecifyCarType
+    {
+        public ISpecifyWheelSize OfType(CarType type);
+    }
+
+    public interface ISpecifyWheelSize
+    {
+        public IBuildCar WithWheels(int size);
+    }
+
+    public interface IBuildCar
+    {
+        public Car Build();
+    }
+
+    public class CarBuilder
+    {
+        public static ISpecifyCarType Create()
+        {
+            return new Impl();
+        }
+
+        private class Impl :
+          ISpecifyCarType,
+          ISpecifyWheelSize,
+          IBuildCar
+        {
+            private Car car = new Car();
+
+            public ISpecifyWheelSize OfType(CarType type)
+            {
+                car.Type = type;
+                return this;
+            }
+
+            public IBuildCar WithWheels(int size)
+            {
+                switch (car.Type)
+                {
+                    case CarType.Crossover when size < 17 || size > 20:
+                    case CarType.Sedan when size < 15 || size > 17:
+                        throw new ArgumentException($"Wrong size of wheel for {car.Type}.");
+                }
+                car.WheelSize = size;
+                return this;
+            }
+
+            public Car Build()
+            {
+                return car;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
+            var car = CarBuilder.Create() //you get ISpecifyCarType
+              .OfType(CarType.Crossover) //you get ISpecifiyWheelSize
+              .WithWheels(18) //you get IBuildCar
+              .Build(); //you get the car
+            Console.WriteLine(car);
         }
     }
 }
