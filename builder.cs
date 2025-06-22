e06d497f6d70888758bd700b155e52f55fd8e98d
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-22 13:29:40 +0200

    Low-level builder

---
+++ builder.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using static System.Console;
+public class Demo
+{
+
+    //we áre going to create a html paragraph
+    static void Main(string[] args)
+    {
+        var hello = "hello";
+
+        //now use the stringbuilder of .net
+        //we are now using a low lwvwl builder taht builds strings
+        var sb = new StringBuilder();
+        sb.Append("</p>");
+        sb.Append(hello);
+        sb.Append("</p>");
+        WriteLine(sb);
+    }
+}
