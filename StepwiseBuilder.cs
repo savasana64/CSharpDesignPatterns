e0ecb36c70f3c4205742dd112d291861f410df2b
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-22 15:22:59 +0200

    StepwiseBuilder --- First Commit
    Add carType and Wheel size. Imagine you can only select a specific WheelSize for a specific car type.

---
+++ StepwiseBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace StepwiseBuilder
+{
+    public enum CarType
+    {
+        Sedan,
+        Crossover
+    };
+    public class Car
+    {
+        public CarType Type;
+        public int WheelSize;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
