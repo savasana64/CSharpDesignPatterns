c7a9276c45dde80ccf0f82f96912d136640a6b42
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-06-22 13:38:59 +0200

    Add more words to html
    As we see too much effort is done here for adding text to html. Something like a html tree is needed...A htmlBuilder would be necessary here.

--- builder.cs
+++ builder.cs
@@ -11,9 +11,21 @@ public class Demo
         //now use the stringbuilder of .net
         //we are now using a low lwvwl builder taht builds strings
         var sb = new StringBuilder();
-        sb.Append("</p>");
+        sb.Append("<p>");
         sb.Append(hello);
         sb.Append("</p>");
         WriteLine(sb);
+
+        var words = new[] { "Hello", "world" };
+        sb.Clear();
+        sb.Append("<ul>");
+        foreach (var word in words)
+        {
+            sb.AppendFormat("<li>{0}</li>" , word);
+        }
+
+        sb.Append("</ul>");
+        WriteLine(sb);
+
     }
 }
