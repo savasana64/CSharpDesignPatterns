35759182204a4cae27a089abbdf24a0f2800704e
savasana64 <128321910+savasana64@users.noreply.github.com>
2025-07-04 20:13:29 +0200

    What are actions?

---
+++ FunctionalBuilder.cs
@@ -0,0 +1,6 @@
+﻿//An Action in C# is a delegate type used to represent a method that returns void and can take zero or more parameters.
+Action sayHello = () => Console.WriteLine("Hello");
+sayHello();  // prints "Hello"
+
+Action<int> printNumber = x => Console.WriteLine(x);
+printNumber(42);  // prints "42"
